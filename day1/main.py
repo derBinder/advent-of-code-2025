bd896e85c526fb03baef76308b778734f76216dc
Felix Binder <felix_binder@icloud.com>
2025-12-19 23:58:49 +0100

    Add day 1 part 1 solution

---
+++ day1/main.py
@@ -0,0 +1,23 @@
+file = open('./input.in').read()
+rows = file.split('\n')
+
+def part1():
+    zero_count = 0
+    position = 50
+
+    for row in rows:
+        direction = row[0]
+        steps = int(row[1:])
+
+        if direction == 'L':
+            position = (position - steps) % 100
+        else:
+            position = (position + steps) % 100
+
+        if position == 0:
+            zero_count += 1
+
+    return zero_count
+
+if __name__ == '__main__':
+    print('Part 1:', part1())
